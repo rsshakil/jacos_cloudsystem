a5c19233b70b833f176e6b0c417a9cba831a7e2f
Md. Mayeen Uddin <mayeennbd@gmail.com>
2020-07-29 12:52:40 +0600

    User table changed

--- app/User.php
+++ app/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
+    protected $table = 'adm_users';
     // public function getFirstNameAttribute() 
     public function getImageAttribute() 
     {
--- database/migrations/2020_07_29_151128_adm_user.php
+++ database/migrations/2020_07_29_151128_adm_user.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class AdmUser extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('adm_users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
