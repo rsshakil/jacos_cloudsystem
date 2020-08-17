5121c7519af2e8f6edc4626791f3c42a18dac26c
Md. Mayeen Uddin <mayeennbd@gmail.com>
2020-08-17 10:42:58 +0600

    cmn_scenerio removed from lv3_jobFactory

--- database/factories/lv3_jobFactory.php
+++ database/factories/lv3_jobFactory.php
@@ -10,7 +10,8 @@ $factory->define(lv3_job::class, function (Faker $faker) {
         'lv3_service_id' => factory(App\lv3_service::class),
         'job_execution_flg' => rand(0,1),
         'execution' => $faker->randomElement(['scenario','batch']),
-        'cmn_scenario_id' => factory(App\cmn_scenario::class),
+        'cmn_scenario_id' => rand(0,5),
+        // 'cmn_scenario_id' => factory(App\cmn_scenario::class),
         'batch_file_path' => $faker->url,
         'append' => $faker->sentence(5),
     ];
