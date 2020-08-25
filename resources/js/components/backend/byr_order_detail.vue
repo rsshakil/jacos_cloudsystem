d30da7618b5fa10abc2e071d528f9b62620981b4
a_sakil <jacossakil@gmail.com>
2020-08-25 11:25:13 +0600

    test delivery

--- resources/js/components/backend/byr_order_detail.vue
+++ resources/js/components/backend/byr_order_detail.vue
@@ -91,22 +91,7 @@
             <thead>
               <tr>
                 <th colspan="100%" style="border: none;">
-                  <select
-                    class="form-control"
-                    name="data_per_page"
-                    id="data_per_page"
-                    style="margin-left: -10px;max-width: 100px; float: left;"
-                  >
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="20">20</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="500">500</option>
-                    <option value="1000">1000</option>
-                  </select>
-
-                  
+          
                 </th>
               </tr>
               <tr>
