0fb5f75a761c66e177e1d34c8394ffcf3d26f168
Md. Mayeen Uddin <mayeennbd@gmail.com>
2020-09-07 11:37:49 +0600

    No test

--- app/Models/CMN/cmn_exchange.php
+++ app/Models/CMN/cmn_exchange.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\CMN;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cmn_scenario extends Model
+class cmn_exchange extends Model
 {
     //
 }
