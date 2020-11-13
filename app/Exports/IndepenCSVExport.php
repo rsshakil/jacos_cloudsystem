87697269d96e90c65a6d4b37172f2a2531ce3969
Md. Mayeen Uddin <mayeennbd@gmail.com>
2020-11-13 16:25:02 +0600

    rename shop_name and shop_code to ship_name etc. git ignore also updated

--- app/Exports/IndepenCSVExport.php
+++ app/Exports/IndepenCSVExport.php
@@ -25,43 +25,33 @@ class IndepenCSVExport implements FromQuery,WithHeadings,WithMapping,ShouldAutoS
     public function headings(): array
     {
         return [
-            'Partner Code', 'Buyer Name Kana',
-            'Saler Name KAna', 'Receive Date', 
-            'Voucher Number','Shop Code',
-            'Shop Name','Receiver Code',
-            'Sale Category','Voucher Category',
+            'Super Code','Buyer Name Kana',
+            'Receiver Code', 'Receiver Name Kana', 'Voucher Number','Constant','Partner Code',
+            'Saler Name Kana','Category Code','Voucher Category',
             'Order Date', 'Expected Delivery Date',
-            'Delivery Service Code', 'Route Code',
-            'Tax Rate','Total Cost Price',
-            'Total Selling Price','List Number',
-            'Jan','Item Name Kana',
-            'Spec Kana','Color',
-            'Size','Inputs',
-            'Order Inputs','Order Quantity',
-            'Cost Price','Selling Price',
+            'Delivery Service Code','Revised Delivery Date',
+            'List Number', 'Jan','Item Name Kana','Size',
+            'Spec Kana','Color','Tax Rate','Order Inputs','Inputs',
+            'Order Quantity','Cost Price','Total Cost Price',
+            'Selling Price','Total Selling Price',
+            
         ];
     }
     public function query()
     {
         $all_indepen_data=byr_order::select(
-            'byr_orders.partner_code',
             'byr_orders.byr_name_kana',
-            // 'byr_orders.slr_name',
+            'byr_orders.partner_code',
             'byr_orders.slr_name_kana',
-            'byr_orders.receive_date',
 
-            'byr_order_vouchers.voucher_number',
-            'byr_order_vouchers.shop_name',
-            'byr_order_vouchers.shop_code',
             'byr_order_vouchers.receiver_code',
-            'byr_order_vouchers.sale_category',
+            'byr_order_vouchers.receiver_name_kana',
+            'byr_order_vouchers.voucher_number',
+            'byr_order_vouchers.category_code',
             'byr_order_vouchers.voucher_category',
             'byr_order_vouchers.order_date',
             'byr_order_vouchers.expected_delivery_date',
             'byr_order_vouchers.delivery_service_code',
-            'byr_order_vouchers.route_code',
-            // 'byr_order_vouchers.receiver_name',
-            // 'byr_order_vouchers.sale_category',
             'byr_order_vouchers.tax_rate',
             'byr_order_vouchers.total_cost_price',
             'byr_order_vouchers.total_selling_price',
@@ -69,21 +59,24 @@ class IndepenCSVExport implements FromQuery,WithHeadings,WithMapping,ShouldAutoS
             'byr_order_items.list_number',
             'byr_order_items.jan',
             'byr_order_items.item_name_kana',
+            'byr_order_items.size',
             'byr_order_items.spec_kana',
             'byr_order_items.color',
-            'byr_order_items.size',
-            'byr_order_items.inputs',
-            // 'byr_order_items.order_unit_quantity',
             'byr_order_items.order_inputs',
+            'byr_order_items.inputs',
             'byr_order_items.order_quantity',
-
-            // 'byr_order_items.cost_unit_price',
-            // 'byr_order_items.selling_unit_price',
             'byr_order_items.cost_price',
             'byr_order_items.selling_price',
+            
+            'byr_buyers.super_code',
+
+            'byr_shipment_vouchers.revised_delivery_date',
         )
         ->join('byr_order_vouchers','byr_order_vouchers.byr_order_id','=','byr_orders.byr_order_id')
         ->join('byr_order_items','byr_order_items.byr_order_voucher_id','=','byr_order_vouchers.byr_order_voucher_id')
+        ->join('cmn_connects','byr_orders.cmn_connect_id','=','cmn_connects.cmn_connect_id')
+        ->join('byr_buyers','byr_buyers.byr_buyer_id','=','cmn_connects.byr_buyer_id')
+        ->join('byr_shipment_vouchers','byr_shipment_vouchers.byr_order_voucher_id','=','byr_order_vouchers.byr_order_voucher_id')
         ->where('byr_orders.byr_order_id', $this->byr_order_id)
         ->orderBy("byr_orders.byr_order_id");
         return $all_indepen_data;
@@ -91,35 +84,34 @@ class IndepenCSVExport implements FromQuery,WithHeadings,WithMapping,ShouldAutoS
     public function map($all_indepen_data): array
     {
         return [
-            $all_indepen_data->partner_code,
+            $all_indepen_data->super_code,
             $this->all_functions->mb_str_pad($all_indepen_data->byr_name_kana, 20),
-            $this->all_functions->mb_str_pad($all_indepen_data->slr_name_kana, 20),
-            date('ymd', strtotime($all_indepen_data->receive_date)),
-            $all_indepen_data->voucher_number,
-            $all_indepen_data->shop_name,
-            str_pad($all_indepen_data->shop_code, 6, '0', STR_PAD_LEFT),
             str_pad($all_indepen_data->receiver_code, 6, '0', STR_PAD_LEFT),
-            $this->all_functions->mb_str_pad($all_indepen_data->sale_category, 2),
+            $all_indepen_data->receiver_name_kana,
+            $all_indepen_data->voucher_number,
+            '00',
+            $all_indepen_data->partner_code,
+            $this->all_functions->mb_str_pad($all_indepen_data->slr_name_kana, 20),
+            $all_indepen_data->category_code,
             $all_indepen_data->voucher_category,
             date('ymd', strtotime($all_indepen_data->order_date)),
             date('ymd', strtotime($all_indepen_data->expected_delivery_date)),
             str_pad($all_indepen_data->delivery_service_code, 3, '0', STR_PAD_LEFT),
-            $all_indepen_data->route_code,
-            $all_indepen_data->tax_rate,
-            $all_indepen_data->total_cost_price,
-            $all_indepen_data->total_selling_price,
+            date('ymd', strtotime($all_indepen_data->revised_delivery_date)),
             str_pad($all_indepen_data->list_number, 2, '0', STR_PAD_LEFT),
             str_pad($all_indepen_data->jan, 13, '0', STR_PAD_LEFT),
             $this->all_functions->mb_str_pad($all_indepen_data->item_name_kana, 25),
+            str_pad($all_indepen_data->size, 5, '0', STR_PAD_LEFT),
             str_pad($all_indepen_data->spec_kana, 5, '0', STR_PAD_LEFT),
             str_pad($all_indepen_data->color, 7, '0', STR_PAD_LEFT),
-            str_pad($all_indepen_data->size, 5, '0', STR_PAD_LEFT),
-            str_pad($all_indepen_data->inputs, 7, '0', STR_PAD_LEFT),
+            $all_indepen_data->tax_rate,
             $all_indepen_data->order_inputs=='ケース'?'CS':($all_indepen_data->order_inputs=='ボール'?'BL':str_repeat(" ",2)),
+            str_pad($all_indepen_data->inputs, 7, '0', STR_PAD_LEFT),
             str_pad(str_replace(".", "", $all_indepen_data->order_quantity), 6, '0', STR_PAD_LEFT),
             $all_indepen_data->cost_price,
+            $all_indepen_data->total_cost_price,
             $all_indepen_data->selling_price,
-            
+            $all_indepen_data->total_selling_price,
         ];
     }
 }
