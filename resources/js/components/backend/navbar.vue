254765d6c468b61baa93a6de035204c9e1230f7f
a_sakil <jacossakil@gmail.com>
2020-11-18 15:49:59 +0600

    master page ui changes

--- resources/js/components/backend/navbar.vue
+++ resources/js/components/backend/navbar.vue
@@ -3,98 +3,43 @@
   <!-- Hi -->
   <!-- {{app.user?app.user.name:'Account'}} -->
   <main class="main-content p-0">
-    <div class="main-navbar sticky-top bg-white">
+    <div class="main-navbar sticky-top bg_custom_color">
       <!-- Main Navbar -->
       <nav class="navbar align-items-stretch navbar-light flex-md-nowrap p-0">
-        <div class="custom_bredcrubs" style="margin-left: 260px">
+        <div class="row" style="margin-right:0;margin-left:0;width:100%;">
+          <div class="col-2">
+                 <div class="site_logo">
+         <img
+              id="main-logo"
+              class="d-inline-block align-top mr-1"
+              style="max-width: 100%;height:60px;"
+              :src="BASE_URL + '/public/backend/images/logo/jacos_logo.png'"
+              alt="Jacos Dashboard"
+            />
+      </div>
+          </div>
+          <div class="col-4">
+           <div class="custom_bredcrubs">
           <Breadcrumbs></Breadcrumbs>
         </div>
-        <form
-          action="#"
-          class="main-navbar__search w-100 d-none d-md-flex d-lg-flex"
-        >
-          <div class="input-group input-group-seamless ml-3">
-            <div class="input-group-prepend">
-              <div class="input-group-text"></div>
-            </div>
-            <input
-              class="navbar-search form-control"
-              type="hidden"
-              placeholder="Search for something..."
-              aria-label="Search"
-            />
           </div>
-        </form>
+          <div class="col-1 p-0">
+
+            <button v-if="user_data.user.company_name!=''" class="btn btn-default byr_list_show">得意先選択</button>
+          </div>
+          <div class="col-5 p-0 ">
+          
         <ul class="navbar-nav top_custom_ul flex-row">
-          <li class="nav-item dropdown top_custom_ul_li">
-            <a
-              class="nav-link dropdown-toggle_backup text-nowrap px-3"
-              href="#"
-              id="navbarDropdown_setting"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <span class="h4">
-                <b-icon
-                  icon="gear-fill"
-                  aria-hidden="true"
-                  style="color: #007BFF"
-                ></b-icon>
-              </span>
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-small"
-              aria-labelledby="navbarDropdown_setting"
-            >
-              <!-- <a class="dropdown-item csv_output" href="#">CSV出力</a>
-              <div class="dropdown-divider"></div>-->
-              <button
-                @click="display_table_col_setting"
-                class="dropdown-item display_item_setting"
-              >
-                表示項目設定
-              </button>
-              <!-- <div class="dropdown-divider"></div>
-              <a class="dropdown-item setting_printing" href="#">印刷</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item csv_import_jan" href="#">CSVインポート JAN</a>
-              <button class="dropdown-item display_arrow_setting">セル移動</button>-->
-            </div>
+          
+        
+          
+          <li class="nav-item" v-if="user_data.user.company_name!=''">
+            <a class="uer_company nav-link top_menu_custom_a"> <b-icon icon="grid3x3-gap-fill"></b-icon> {{ user_data.user.company_name!='' ? user_data.user.company_name : "" }}</a>
           </li>
-          <li class="nav-item border-left dropdown">
-            <a
-              class="nav-link dropdown-toggle text-nowrap px-3"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <span class="flag-icon flag-icon-jp" v-if="local == 'ja'"></span>
-              <span class="flag-icon flag-icon-us" v-if="local == 'en'"></span>
-              {{ local == "en" ? "English" : "日本語" }}
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-small"
-              aria-labelledby="navbarDropdown"
-            >
-              <a class="dropdown-item" :href="BASE_URL + '/language/en'">
-                <span class="flag-icon flag-icon-us"></span>
-                English
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" :href="BASE_URL + '/language/ja'">
-                <span class="flag-icon flag-icon-jp"></span>
-                日本語
-              </a>
-            </div>
-          </li>
+          
           <li class="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle text-nowrap px-3"
+              class="nav-link top_menu_custom_a dropdown-toggle text-nowrap px-3"
               data-toggle="dropdown"
               href="#"
               role="button"
@@ -102,17 +47,18 @@
               aria-expanded="false"
             >
               <!-- @if(Auth::user()->image) -->
+              <!--
               <img
                 class="user-avatar rounded-circle mr-2"
                 style="max-height: 46px !important"
                 v-if="user_data.user"
                 :src="imageSrc()"
                 alt
-              />
+              />-->
               <!-- <img class="rounded-circle" v-if="user_image" :src="user_image" alt="No image" width="110" /> -->
               <!-- @endif -->
               <span class="d-none d-md-inline-block">
-                {{ user_data.user ? user_data.user.name : "" }}
+                <b-avatar variant=""></b-avatar> {{ user_data.user ? user_data.user.name : "" }}
               </span>
             </a>
             <div
@@ -161,13 +107,87 @@
               <div class="dropdown-divider"></div>
               <!-- <a class="dropdown-item text-danger" href="#">
               <i class="material-icons text-danger">&#xE879;</i> Logout </a>-->
-              <button class="dropdown-item text-danger" @click="logout()">
-                <i class="material-icons text-danger">&#xE879;</i>
+              
+            </div>
+          </li>
+            <li class="nav-item">
+            <button class="btn btn-defalut logout_btn text-danger" @click="logout()">
+                
                 Logout
               </button>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle_backup text-nowrap px-3"
+              href="#"
+              id="navbarDropdown_setting"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span class="h4">
+                <b-icon
+                  icon="gear-fill"
+                  aria-hidden="true"
+                  style="color: #fff"
+                ></b-icon>
+              </span>
+            </a>
+            <div
+              class="dropdown-menu dropdown-menu-small"
+              aria-labelledby="navbarDropdown_setting"
+            >
+              <!-- <a class="dropdown-item csv_output" href="#">CSV出力</a>
+              <div class="dropdown-divider"></div>-->
+              <button
+                @click="display_table_col_setting"
+                class="dropdown-item display_item_setting"
+              >
+                表示項目設定
+              </button>
+              <!-- <div class="dropdown-divider"></div>
+              <a class="dropdown-item setting_printing" href="#">印刷</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item csv_import_jan" href="#">CSVインポート JAN</a>
+              <button class="dropdown-item display_arrow_setting">セル移動</button>-->
+            </div>
+          </li>
+         
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link top_menu_custom_a dropdown-toggle text-nowrap px-3"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <span class="flag-icon flag-icon-jp" v-if="local == 'ja'"></span>
+              <span class="flag-icon flag-icon-us" v-if="local == 'en'"></span>
+              {{ local == "en" ? "English" : "日本語" }}
+            </a>
+            <div
+              class="dropdown-menu dropdown-menu-small"
+              aria-labelledby="navbarDropdown"
+            >
+              <a class="dropdown-item" :href="BASE_URL + '/language/en'">
+                <span class="flag-icon flag-icon-us"></span>
+                English
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" :href="BASE_URL + '/language/ja'">
+                <span class="flag-icon flag-icon-jp"></span>
+                日本語
+              </a>
             </div>
           </li>
         </ul>
+          </div>
+        </div>
+       
+  
         <nav class="nav">
           <a
             href="#"
