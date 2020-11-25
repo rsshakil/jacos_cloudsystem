5e94ec180224fedfc2b1e498793f1b811b400d04
Md. Mayeen Uddin <mayeennbd@gmail.com>
2020-11-25 13:27:55 +0600

    Buyer view for saller with order count

--- resources/js/components/backend/byr_slr_list_with_order.vue
+++ resources/js/components/backend/byr_slr_list_with_order.vue
@@ -1,44 +1,51 @@
 <template>
-<div>
-     <div class="row">
-        <div class="col-12" v-role="['Slr','Byr']">
-            <div v-for="order_item in slr_order_list" :key="order_item.byr_buyer_id">
-                 <b-button variant="outline-primary">{{order_item.buyer_name}} &nbsp &nbsp {{order_item.total_order}}件</b-button>
-            </div>
-        </div>
+  <div>
+    <div class="row">
+      <div class="col-12" v-role="['Slr', 'Byr']">
+        <ul class="buyer_button_list">
+          <li
+            v-for="order_item in slr_order_list"
+            :key="order_item.byr_buyer_id"
+          >
+            <b-button variant="outline-primary"
+              >{{ order_item.buyer_name }} &nbsp &nbsp
+              {{ order_item.total_order }}件</b-button
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-</div>
-
+  </div>
 </template>
 
 <script>
-    export default {
-
-        data(){
-            return {
-               'byr_slr_list':'',
-               slr_order_list:null,
-            }
-        },
-        methods:{
-            loadData(){
-                axios.post(this.myLang.base_url+'api/get_byr_slr_data',{user_id:this.myLang.user_info_id})
-                .then(({data})=>{
-                    // console.log("Hi");
-                    console.log(data);
-                    this.slr_order_list=data.slr_order_info;
-                })
-            }
-            
-        },
-          created() {
-      
-      console.log('created jacos management log');
+export default {
+  data() {
+    return {
+      byr_slr_list: "",
+      slr_order_list: null,
+    };
   },
-        mounted() {
-            this.init();
-            console.log('Home Component mounted.')
-            this.loadData();
-        }
-    }
+  methods: {
+    loadData() {
+      axios
+        .post(this.myLang.base_url + "api/get_byr_slr_data", {
+          user_id: this.myLang.user_info_id,
+        })
+        .then(({ data }) => {
+          // console.log("Hi");
+          console.log(data);
+          this.slr_order_list = data.slr_order_info;
+        });
+    },
+  },
+  created() {
+    console.log("created jacos management log");
+  },
+  mounted() {
+    this.init();
+    console.log("Home Component mounted.");
+    this.loadData();
+  },
+};
 </script>
