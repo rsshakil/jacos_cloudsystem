5746ab9a3ad9d83c11643b7143670beed6d12534
a_sakil <jacossakil@gmail.com>
2020-11-25 15:03:10 +0600

    byr selected page added

---
+++ resources/js/components/backend/SLR/blog_view_for_select_customer.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+<!--bloog section-->
+        <div class="row">
+            <div class="col-12">
+           
+                <b-container class="bv-example-row">
+      
+<!--user blog-->       
+  <b-row v-can="['slr_view']">
+    <b-col v-if="user_blog.length!='0'">      
+  <h4 class="my-3 blog_titles"><i class="fas custom_blog_square fa-square-full"></i> {{user_blog.blog_title}}</h4>
+    <div class="blogs_content" v-html="user_blog.blog_content">
+    </div>
+    </b-col>
+  </b-row>
+  <!--user blog end-->  
+   <!--admin blog-->           
+  <b-row>
+    <b-col v-if="single_blog.length!='0'">                    
+
+  <h4 class="my-3 blog_titles"><i class="fas custom_blog_square fa-square-full"></i>  {{single_blog.blog_title}}</h4>
+  <!--<p class="created_at">Created at @{{ single_blog.created_at | ja_date_time }}</p>-->
+  <!--<b-img v-if="single_blog.feature_img!='null'" :src="BASE_URL+'storage/app/public/backend/images/blog_images/'+single_blog.feature_img" fluid-grow alt="Fluid-grow image"></b-img>-->
+    <div class="blogs_content" v-html="single_blog.blog_content">
+    </div>
+    </b-col>
+  </b-row>
+      <!--admin blog end-->  
+  <!--
+  <b-row>
+    <b-col cols="12" v-for="(value) in blog_lists" :key="value.cmn_blog_id">                    
+
+  <h5 class="my-3 blog_titles"><i class="fas custom_blog_square fa-square-full"></i> {{value.blog_title}}</h5>
+  <p class="created_at">Created at @ {{ value.created_at | diffForHumans }}</p>-->
+  <!--<b-img v-if="value.feature_img!=null" :src="BASE_URL+'storage/app/public/backend/images/blog_images/'+value.feature_img" fluid-grow alt="Fluid-grow image"></b-img>
+  <div class="blogs_content" v-html="value.blog_content"></div>
+  </b-col>
+   
+  </b-row>
+-->
+</b-container>
+
+
+                </div>
+        </div>
+</div>
+
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return {
+                home_text:Globals.welcome_text,
+                BASE_URL:Globals.base_url,
+                activeVal: 'home',
+                blog_lists:{},
+                single_blog:{},
+                user_blog:{},
+            }
+        },
+        methods:{
+            get_all_blogs(){
+                axios.get(this.BASE_URL +"api/get_all_published_blog_list").then((data) => {
+                    this.blog_lists = data.data.blog_list;
+                });
+            },
+            get_signle_top_blog(){
+                axios.get(this.BASE_URL +"api/get_signle_top_blog").then((data) => {
+                    this.single_blog = data.data.blog_list;
+                });
+            },
+            get_user_top_blog(){
+                axios.get(this.BASE_URL +"api/get_user_top_blog").then((data) => {
+                    this.user_blog = data.data.blog_list;
+                });
+            },
+
+        },
+          created() {
+      this.get_all_blogs();
+      this.get_signle_top_blog();
+      this.get_user_top_blog();
+      Fire.$on("AfterCreateblog", () => {
+        this.get_all_blogs();
+    });
+  },
+        mounted() {
+            this.init();
+            console.log('Home blog Component mounted.')
+        }
+    }
+</script>
---
+++ resources/js/components/backend/SLR/selected_byr_page.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="main-content-container container-fluid px-4">
+    <!-- Page Header -->
+    <br>
+        <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-3">
+    
+
+
+                <page_header_default></page_header_default>
+               <!-- <byr_slr_list_with_order></byr_slr_list_with_order>-->
+                <blog_view_for_select_customer></blog_view_for_select_customer>
+
+
+            </div>
+
+
+
+        </div>
+</div>
+</template>
+
+<script>
+ import page_header_default from '../page_header_default'
+ import byr_slr_list_with_order from '../byr_slr_list_with_order'
+ import blog_view_for_select_customer from './blog_view_for_select_customer'
+    export default {
+        components : {
+    'page_header_default': page_header_default,
+    'byr_slr_list_with_order': byr_slr_list_with_order,
+    'blog_view_for_select_customer': blog_view_for_select_customer,
+  },
+        data(){
+            return {
+                home_title:'',
+            }
+        },
+        methods:{
+           
+        },
+          created() {
+                
+            },
+        mounted() {
+            this.init();
+            console.log('Home Component mounted.')
+        }
+    }
+</script>
