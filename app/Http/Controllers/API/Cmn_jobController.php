c43bd9dc81b9d08c0b9a5063260328ae0e2ab1d8
y_sakaki <sakaki@jacos.co.jp>
2020-12-17 11:28:41 +0900

    spr_code to super_code

--- app/Http/Controllers/API/Cmn_jobController.php
+++ app/Http/Controllers/API/Cmn_jobController.php
@@ -24,12 +24,12 @@ class Cmn_jobController extends Controller
     public function index()
     {
         // $byr_company_list = byr_buyer::get()->byr_company;
-        $byr_company_list = byr_buyer::select('byr_buyers.super_code','cmn_companies.company_name','cmn_companies.cmn_company_id')
-        ->join('cmn_companies','cmn_companies.cmn_company_id','=','byr_buyers.cmn_company_id')->get();
-        $slr_company_list = slr_seller::select('cmn_companies.company_name','cmn_companies.jcode','cmn_companies.cmn_company_id')
-        ->join('cmn_companies','cmn_companies.cmn_company_id','=','slr_sellers.cmn_company_id')->get();
+        $byr_company_list = byr_buyer::select('byr_buyers.super_code', 'cmn_companies.company_name', 'cmn_companies.cmn_company_id')
+        ->join('cmn_companies', 'cmn_companies.cmn_company_id', '=', 'byr_buyers.cmn_company_id')->get();
+        $slr_company_list = slr_seller::select('cmn_companies.company_name', 'cmn_companies.jcode', 'cmn_companies.cmn_company_id')
+        ->join('cmn_companies', 'cmn_companies.cmn_company_id', '=', 'slr_sellers.cmn_company_id')->get();
 
-        $job_list = cmn_job::select('cmn_jobs.*','cmn_scenarios.*','cmn_companies.company_name as byr_company','byr_buyers.super_code','slr_companies.company_name as slr_company','slr_companies.jcode')
+        $job_list = cmn_job::select('cmn_jobs.*', 'cmn_scenarios.*', 'cmn_companies.company_name as byr_company', 'byr_buyers.super_code', 'slr_companies.company_name as slr_company', 'slr_companies.jcode')
         ->join('cmn_scenarios', 'cmn_jobs.cmn_scenario_id', '=', 'cmn_scenarios.cmn_scenario_id')
         ->join('cmn_connects', 'cmn_connects.cmn_connect_id', '=', 'cmn_jobs.cmn_connect_id')
         ->join('byr_buyers', 'byr_buyers.byr_buyer_id', '=', 'cmn_connects.byr_buyer_id')
@@ -93,7 +93,7 @@ class Cmn_jobController extends Controller
         \Log::debug('scenario exec start---------------');
         // user info check
         \Log::debug($request);
-        $user = DB::table('adm_users')->where('email',$request->email)->first();
+        $user = DB::table('adm_users')->where('email', $request->email)->first();
         if (!$user) {
             return ['status'=>1, 'message' => 'Authentication faild!'];
         }
@@ -109,7 +109,7 @@ class Cmn_jobController extends Controller
         // $sc=null;
         // return $request->all();
         // if (isset($request['spr_code'])) {
-            // return $request->all();
+        // return $request->all();
         // }
         // return $array = json_decode(json_encode($request->all()), true);
         // // return array_keys((array) $request);
@@ -119,26 +119,25 @@ class Cmn_jobController extends Controller
         //     return "Not Found";
         // }
         $request_all=$request->all();
-        if (array_key_exists("spr_code",$request_all) && array_key_exists("partner_code",$request_all) && array_key_exists("class",$request_all)) {
-            
-            $spr_code=$request->spr_code;
+        if (array_key_exists("super_code", $request_all) && array_key_exists("partner_code", $request_all) && array_key_exists("class", $request_all)) {
+            $super_code=$request->super_code;
             $partner_code=$request->partner_code;
             $class=$request->class;
-            $sc=cmn_job::select('cmn_jobs.cmn_connect_id','cmn_scenarios.*')
+            $sc=cmn_job::select('cmn_jobs.cmn_connect_id', 'cmn_scenarios.*')
             ->join('cmn_scenarios', 'cmn_jobs.cmn_scenario_id', '=', 'cmn_scenarios.cmn_scenario_id')
-            ->join('cmn_connects','cmn_connects.cmn_connect_id','=','cmn_jobs.cmn_connect_id')
-            ->join('byr_buyers','cmn_connects.byr_buyer_id','=','byr_buyers.byr_buyer_id')
+            ->join('cmn_connects', 'cmn_connects.cmn_connect_id', '=', 'cmn_jobs.cmn_connect_id')
+            ->join('byr_buyers', 'cmn_connects.byr_buyer_id', '=', 'byr_buyers.byr_buyer_id')
             ->where([
-                ['byr_buyers.spr_code','=',$spr_code],
+                ['byr_buyers.super_code','=',$super_code],
                 ['cmn_connects.partner_code','=',$partner_code],
                 ['cmn_jobs.class','=',$class],
             ])
             ->first();
-        }else if (array_key_exists("cmn_job_id",$request_all)){
+        } elseif (array_key_exists("cmn_job_id", $request_all)) {
             $cmn_job_id=$request->cmn_job_id;
-            $sc = cmn_job::select('cmn_jobs.cmn_connect_id','cmn_scenarios.*')
+            $sc = cmn_job::select('cmn_jobs.cmn_connect_id', 'cmn_scenarios.*')
             ->join('cmn_scenarios', 'cmn_jobs.cmn_scenario_id', '=', 'cmn_scenarios.cmn_scenario_id')
-            ->where('cmn_jobs.cmn_job_id', $cmn_job_id)->where('cmn_jobs.is_active',1)->first();
+            ->where('cmn_jobs.cmn_job_id', $cmn_job_id)->where('cmn_jobs.is_active', 1)->first();
         }
         // return $sc;
         \Log::info($sc);
@@ -152,18 +151,18 @@ class Cmn_jobController extends Controller
         
         // $sc_obj = new ouk_order_toj();//$sc->file_path;
         $customClassPath = "\\App\\";
-        $nw_f_pth = explode('/',$sc->file_path);
-        foreach($nw_f_pth as $p){
+        $nw_f_pth = explode('/', $sc->file_path);
+        foreach ($nw_f_pth as $p) {
             $customClassPath .= $p.'\\';
         }
-        $customClassPath = rtrim($customClassPath,"\\");
+        $customClassPath = rtrim($customClassPath, "\\");
         $sc_obj = new $customClassPath;
         // シナリオ実行
         if (!method_exists($sc_obj, 'exec')) {
             \Log::error('scenario exec error');
             return ['status'=>'1','message'=>'Scenario exec function is not exist!'];
         }
-        $ret = $sc_obj->exec($request,$sc);
+        $ret = $sc_obj->exec($request, $sc);
         if ($ret !== 0) {
             // error
             \Log::debug('scenario exec error');
@@ -178,8 +177,9 @@ class Cmn_jobController extends Controller
     }
 
    
-    public function slr_job_list_by_seller_id($slr_seller_id){
-        $sc = cmn_job::select('cmn_jobs.*','cmn_scenarios.*')
+    public function slr_job_list_by_seller_id($slr_seller_id)
+    {
+        $sc = cmn_job::select('cmn_jobs.*', 'cmn_scenarios.*')
         ->join('cmn_scenarios', 'cmn_jobs.cmn_scenario_id', '=', 'cmn_scenarios.cmn_scenario_id')
         ->join('cmn_connects', 'cmn_connects.cmn_connect_id', '=', 'cmn_jobs.cmn_connect_id')
         ->where('cmn_connects.slr_seller_id', $slr_seller_id)->get();
