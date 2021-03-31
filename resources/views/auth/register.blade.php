ca219fadaacb1415b4e4d752f613cf686580bc04
Mayeen <jacosmayeenuddin@gmail.com>
2021-03-31 11:09:10 +0600

    No test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{Config::get('app.url').'register'}}">
+                    <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
