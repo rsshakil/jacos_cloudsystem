e7dced42acc68f6c04314c6a5566929ab2b9ce4d
Mayeen <jacosmayeenuddin@gmail.com>
2021-03-31 10:27:19 +0600

    asset to config path changed

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('public/css/auth/app.js') }}" defer></script>
+    <script src="{{Config::get('app.url').'public/css/auth/app.js'}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('public/css/auth/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{Config::get('app.url').'public/css/auth/app.css'}}">
 </head>
 <body>
     <div id="app">
