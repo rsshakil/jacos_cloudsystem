263cb7e7d901652fb5bfef30f60b9a7be8339cc7
Mayeen <jacosmayeenuddin@gmail.com>
2021-04-02 10:42:22 +0600

    Local test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         // $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
+        // \Log::info(env('APP_URL', null));
+        \Log::info("Config ".\Config::get('app.env'));
+        // env('MY_VALUE', 'default_value');
         if(\App::environment() !== 'local') {
             URL::forceScheme('https');
             \Log::info('https');
