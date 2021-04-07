b3fa3bdefb212f74303ce780a6cbe29a6d8f525e
Mayeen <jacosmayeenuddin@gmail.com>
2021-04-07 10:26:56 +0600

    Invoice command start date and end date show

--- app/Console/Commands/InvoiceCommand.php
+++ app/Console/Commands/InvoiceCommand.php
@@ -45,24 +45,29 @@ class InvoiceCommand extends Command
      */
     public function handle()
     {
+        \Log::info("----Starting----");
         $today=date('y-m-d');
         $cmn_connect_info=cmn_connect::select('optional')->where('cmn_connect_id',1)->first();
         $optional=json_decode($cmn_connect_info->optional);
         $closing_date_array=$optional->invoice->closing_date;
         sort($closing_date_array);
-
+        // $this->comment("closing_date_array ".$closing_date_array);
         $closing_date_array=$this->all_used_fun->arra_sorting($closing_date_array);
         $closing_date_count=count($closing_date_array);
         $start_date="00:00:00";
         $end_date="00:00:00";
-
+        // \Log::info($closing_date_array);
         foreach ($closing_date_array as $key => $closing_day) {
-
+            // $this->comment("closing_day ".$closing_day);
             $closing_date= $this->all_used_fun->closing_date($closing_day);
+            // $this->comment("closing_date ".$closing_date);
+            // $this->comment("Today ".$today);
             if ($closing_date==$today) {
                 $end_date=$closing_date;
                 if ($closing_date_count>1) {
                     $array_key=array_search($closing_day,$closing_date_array);
+                    // \Log::info("Date Key ".$array_key);
+                    // $this->comment("Date Key ".$array_key);
                     if ($array_key==0) {
                         $array_end_date=$this->all_used_fun->closing_date(end($closing_date_array));
                         $start_date=$this->all_used_fun->first_start_date($array_end_date,$closing_date);
@@ -71,11 +76,14 @@ class InvoiceCommand extends Command
                     }
                 }else{
                     $start_date=$this->all_used_fun->start_date($closing_date,1);
+                    // $this->comment("Else date ".$start_date);
                 }
                 // Matched
-                \Log::info("Start Date: ".$start_date);
-                \Log::info("End Date: ".$end_date);
+                // \Log::info("Start Date: ".$start_date);
+                // \Log::info("End Date: ".$end_date);
                 $this->comment("Invoice Running.....");
+                $this->comment("Start Date: ".$start_date);
+                $this->comment("End Date: ".$end_date);
                 $this->invoice->invoiceScheduler($start_date,$end_date);
                 $this->comment("Done");
                 // Matched
