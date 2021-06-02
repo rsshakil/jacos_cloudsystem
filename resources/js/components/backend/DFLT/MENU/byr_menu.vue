4cbf6c78b2766ba47cc0728c06e9a510495e1f1a
a_sakil <jacossakil@gmail.com>
2021-06-02 13:23:07 +0600

    byr menu create only

--- resources/js/components/backend/DFLT/MENU/byr_menu.vue
+++ resources/js/components/backend/DFLT/MENU/byr_menu.vue
@@ -34,6 +34,55 @@
               </ul>
               </div>
               </li>
+
+<!--new slr menu for byr-->
+<li class="nav-item">
+          <a
+            class="nav-link collapsed"
+            href="#slrDatamenu1"
+            data-toggle="collapse"
+            data-target="#slrDatamenu1"
+            ><b-icon icon="gear-fill" font-scale="1.2"></b-icon>
+            問屋データ</a
+          >
+          <div class="collapse" id="slrDatamenu1" aria-expanded="false">
+            <ul class="flex-column pl-2 nav">
+            <li class="nav-item" v-can="['byr_view']">
+                <router-link to="/order_list" class="nav-link">
+                  <b-icon icon="receipt" font-scale="1.2"></b-icon>
+                 受注・出荷業務
+                </router-link>
+              </li>
+               <li class="nav-item" v-can="['byr_view']">
+                <router-link to="/order_receive" class="nav-link">
+                  <b-icon icon="card-checklist" font-scale="1.2"></b-icon>
+                 受領確認
+                </router-link>
+              </li>
+              <li class="nav-item" v-can="['byr_view']">
+                <router-link to="/return_list" class="nav-link">
+                  <b-icon icon="card-checklist" font-scale="1.2"></b-icon>
+                 返品確認
+                </router-link>
+              </li>
+             
+              <li class="nav-item" v-can="['byr_view']">
+                <router-link to="/invoice_list" class="nav-link">
+                  <b-icon icon="card-checklist" font-scale="1.2"></b-icon>
+                  請求業務
+                </router-link>
+              </li>
+              <li class="nav-item" v-can="['byr_view']">
+                <router-link to="/payment_list" class="nav-link">
+                  <b-icon icon="card-checklist" font-scale="1.2"></b-icon>
+                支払確認
+                </router-link>
+              </li>
+            </ul>
+              </div>
+              </li>
+<!--new slr menu for byr-->
+
     </ul>
 </div>
 </template>
