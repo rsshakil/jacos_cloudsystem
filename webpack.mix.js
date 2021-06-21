4d9839d8ff34f4ee382f9fa341e8799480b6d457
Mayeen <jacosmayeenuddin@gmail.com>
2021-06-21 17:03:13 +0600

    mix app_url changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+require('dotenv').config();
+const app_url = process.env.APP_URL;
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,7 +14,9 @@ const mix = require('laravel-mix');
 // Override mix internal webpack output configuration
 mix.config.webpackConfig.output = {
     chunkFilename: 'js/build_component/[name].js',
-    publicPath: '/public/',
+    // publicPath: '/public/', //For server
+    publicPath: app_url + 'public/',
+    // publicPath: '/public/',
 };
 
 mix.js('resources/js/app.js', 'public/js')
