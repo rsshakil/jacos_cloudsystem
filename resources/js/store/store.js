666fbbed8d04f1944c41d6bac4e95e4114f1a10f
Mayeen <jacosmayeenuddin@gmail.com>
2021-06-29 09:50:04 +0600

    Order details module added

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import orderModule from "./modules/DATA/orderModule"
+import orderDetailsModule from "./modules/DATA/orderDetailsModule"
 import defaultModule from "./modules/DEFAULT/defaultModule"
 Vue.use(Vuex);
 export default new Vuex.Store({
     modules: {
         orderModule,
+        orderDetailsModule,
         defaultModule
     }
 })
